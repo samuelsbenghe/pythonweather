20007848f4de09ff4ea36f4fb89abaae2011a95c
Edith-Samuel Sbenghe <72344412+samuelsbenghe@users.noreply.github.com>
2023-11-21 17:42:44 +0000

    Update natty.py

--- natty.py
+++ natty.py
@@ -2,7 +2,7 @@ import internal.openweathermap as owm
 
 
 def main():
-    weather_service = owm.Weather("3b0b63e5af21bd69a76d77b43d247827")
+    weather_service = owm.Weather("")
     current_weather_data_client = weather_service.current_weather_data()
 
     city = input("Enter city name: ")
